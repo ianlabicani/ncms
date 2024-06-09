a93c71ed6df99eac8cfb3278ff5896d571e44eb2
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-06-09 15:21:49 +0800

    change the form design

--- resources/views/patients/update-patient.blade.php
+++ resources/views/patients/update-patient.blade.php
@@ -1,88 +1,192 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Edit Patient')
+
+@section('css')
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ url('Image/logo.png') }}" type="image/x-icon">
+    {{-- Boostrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+    {{-- Alert  --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <style>
+        body {
+            font-family: 'Nunito'
+        }
+
+        .color {
+            color: #039e43;
+        }
+
+        .colored-toast.swal2-icon-success {
+            background-color: #039e43 !important;
+        }
+
+        .colored-toast.swal2-icon-error {
+            background-color: #f27474 !important;
+        }
+
+        .colored-toast.swal2-icon-warning {
+            background-color: #DC3545 !important;
+        }
+
+        .colored-toast.swal2-icon-info {
+            background-color: #3fc3ee !important;
+        }
+
+        .colored-toast.swal2-icon-question {
+            background-color: #87adbd !important;
+        }
+
+        .colored-toast .swal2-title {
+            color: white;
+        }
+
+        .colored-toast .swal2-close {
+            color: white;
+        }
+
+        .colored-toast .swal2-html-container {
+            color: white;
+        }
+    </style>
+@stop
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h4 class="fw-semibold color">Edit Patient Information</h4>
 @stop
 
 @section('content')
-<body>
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Edit Patient</div>
-                    <div class="card-body">
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-                        <form action="{{ route('patients.update', $patient) }}" method="POST">
-                            @csrf
-                            @method('PUT')
 
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" name="first_name" id="first_name" class="form-control" value="{{ old('first_name', $patient->first_name) }}" required>
-                            </div>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header fw-semibold bg-success">Change Patient Information
+                        </div>
+                        <div class="card-body">
+                            @if (session('successupdate'))
+                                <script>
+                                    const Toast = Swal.mixin({
+                                        toast: true,
+                                        position: 'top-end',
+                                        iconColor: 'white',
+                                        customClass: {
+                                            popup: 'colored-toast',
+                                        },
+                                        showConfirmButton: false,
+                                        timer: 3000,
+                                        timerPr0ogressBar: true,
+                                    });
+                                    (async () => {
+                                        await Toast.fire({
+                                            icon: 'success',
+                                            title: 'Patient Information Changed'
+                                        })
+                                    })()
+                                </script>
+                            @endif
+                            <form action="{{ route('patients.update', $patient) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="first_name">First Name</label>
+                                            <input type="text" name="first_name" id="first_name" class="form-control"
+                                                value="{{ old('first_name', $patient->first_name) }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="last_name">Last Name</label>
+                                            <input type="text" name="last_name" id="last_name" class="form-control"
+                                                value="{{ old('last_name', $patient->last_name) }}" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="date_of_birth">Date of Birth</label>
+                                            <input type="date" name="date_of_birth" id="date_of_birth"
+                                                class="form-control"
+                                                value="{{ old('date_of_birth', $patient->date_of_birth) }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="gender">Gender</label>
+                                            <select name="gender" id="gender" class="form-control" required>
+                                                <option value="male"
+                                                    {{ old('gender', $patient->gender) == 'male' ? 'selected' : '' }}>Male
+                                                </option>
+                                                <option value="female"
+                                                    {{ old('gender', $patient->gender) == 'female' ? 'selected' : '' }}>
+                                                    Female
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
 
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" name="last_name" id="last_name" class="form-control" value="{{ old('last_name', $patient->last_name) }}" required>
-                            </div>
+                                        <div class="form-group">
+                                            <label for="contact_number">Contact Number</label>
+                                            <input type="text" name="contact_number" id="contact_number"
+                                                class="form-control"
+                                                value="{{ old('contact_number', $patient->contact_number) }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="email" name="email" id="email" class="form-control"
+                                                value="{{ old('email', $patient->email) }}" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
 
-                            <div class="form-group">
-                                <label for="date_of_birth">Date of Birth</label>
-                                <input type="date" name="date_of_birth" id="date_of_birth" class="form-control" value="{{ old('date_of_birth', $patient->date_of_birth) }}" required>
-                            </div>
+                                        <div class="form-group">
+                                            <label for="address">Address</label>
+                                            <input type="text" name="address" id="address" class="form-control"
+                                                value="{{ old('address', $patient->address) }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="registration_date">Registration Date</label>
+                                            <input type="date" name="registration_date" id="registration_date"
+                                                class="form-control"
+                                                value="{{ old('registration_date', $patient->registration_date) }}"
+                                                required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary px-5">Update</button>
+                                    <a href="{{ url('/patients') }}" class="btn btn-danger text-decoration-none">Back</a>
+                                </div>
 
-                            <div class="form-group">
-                                <label for="gender">Gender</label>
-                                <select name="gender" id="gender" class="form-control" required>
-                                    <option value="male" {{ old('gender', $patient->gender) == 'male' ? 'selected' : '' }}>Male</option>
-                                    <option value="female" {{ old('gender', $patient->gender) == 'female' ? 'selected' : '' }}>Female</option>
-                                    <option value="other" {{ old('gender', $patient->gender) == 'other' ? 'selected' : '' }}>Other</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="contact_number">Contact Number</label>
-                                <input type="text" name="contact_number" id="contact_number" class="form-control" value="{{ old('contact_number', $patient->contact_number) }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $patient->email) }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="address">Address</label>
-                                <input type="text" name="address" id="address" class="form-control" value="{{ old('address', $patient->address) }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="registration_date">Registration Date</label>
-                                <input type="date" name="registration_date" id="registration_date" class="form-control" value="{{ old('registration_date', $patient->registration_date) }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">Update</button>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@stop
+        </div>
+    @stop
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
-
-@section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-@stop
+    @section('js')
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+    @stop
