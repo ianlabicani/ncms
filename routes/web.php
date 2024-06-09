a6da3e7f7ebdfd40441979545535f41e7cb49f0e
Ian Labicani <labicaniian@gmail.com>
2024-06-09 10:12:33 +0800

    change alias to patients.index

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/patients', [App\Http\Controllers\PatientController::class, 'index'])->name('patients');
+Route::get('/patients', [App\Http\Controllers\PatientController::class, 'index'])->name('patients.index');
