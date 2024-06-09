4f2038aa6b22c59b35f53f78b1e67575cfaecbed
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-06-09 15:20:51 +0800

    change the form design

--- resources/views/patients/add-patient.blade.php
+++ resources/views/patients/add-patient.blade.php
@@ -1,88 +1,184 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Add Patient')
+
+@section('css')
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ url('Image/logo.png') }}" type="image/x-icon">
+    {{-- Boostrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+    {{-- Alert  --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <style>
+        body {
+            font-family: 'Nunito'
+        }
+
+        .color {
+            color: #039e43;
+        }
+
+        .colored-toast.swal2-icon-success {
+            background-color: #039e43 !important;
+        }
+
+        .colored-toast.swal2-icon-error {
+            background-color: #f27474 !important;
+        }
+
+        .colored-toast.swal2-icon-warning {
+            background-color: #DC3545 !important;
+        }
+
+        .colored-toast.swal2-icon-info {
+            background-color: #3fc3ee !important;
+        }
+
+        .colored-toast.swal2-icon-question {
+            background-color: #87adbd !important;
+        }
+
+        .colored-toast .swal2-title {
+            color: white;
+        }
+
+        .colored-toast .swal2-close {
+            color: white;
+        }
+
+        .colored-toast .swal2-html-container {
+            color: white;
+        }
+    </style>
+@stop
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h4 class="fw-bolder mb-3 color">Add Patient</h4>
+    @if (session('successadded'))
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                iconColor: 'white',
+                customClass: {
+                    popup: 'colored-toast',
+                },
+                showConfirmButton: false,
+                timer: 3000,
+                timerPr0ogressBar: true,
+            });
+            (async () => {
+                await Toast.fire({
+                    icon: 'success',
+                    title: 'Patient Added'
+                })
+            })()
+        </script>
+    @endif
 @stop
 
 @section('content')
-<body>
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Create Patient</div>
-                    <div class="card-body">
-                        @if (session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
+
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body px-4">
+                            <form action="{{ route('add.patient') }}" class="text-dark" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="first_name">First Name</label>
+                                            <input type="text" name="first_name" id="first_name" class="form-control"
+                                                placeholder="Enter your First Name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="last_name">Last Name</label>
+                                            <input type="text" name="last_name" id="last_name" class="form-control"
+                                                placeholder="Enter your Last Name" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="date_of_birth">Date of Birth</label>
+                                            <input type="date" name="date_of_birth" id="date_of_birth"
+                                                class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="gender">Gender</label>
+                                            <select name="gender" id="gender" class="form-control" required>
+                                                <option value="Male">Male</option>
+                                                <option value="Female">Female</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="contact_number">Contact Number</label>
+                                            <input type="text" name="contact_number" id="contact_number"
+                                                class="form-control" placeholder="Phone Number" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="email" name="email" id="email" class="form-control"
+                                                placeholder="example@example.com" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="address">Address</label>
+                                            <input type="text" name="address" id="address" class="form-control"
+                                                placeholder="Address" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="registration_date">Registration Date</label>
+                                            <input type="date" name="registration_date" id="registration_date"
+                                                class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <div class="d-flex align-items-center">
+                                            <button type="submit" class="btn btn-primary me-3">Add
+                                                Patient</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    @endif
-                        <form action="{{ route('add.patient') }}" method="POST">
-                            @csrf
-
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="date_of_birth">Date of Birth</label>
-                                <input type="date" name="date_of_birth" id="date_of_birth" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="gender">Gender</label>
-                                <select name="gender" id="gender" class="form-control" required>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="contact_number">Contact Number</label>
-                                <input type="text" name="contact_number" id="contact_number" class="form-control" placeholder="Contact Number" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="address">Address</label>
-                                <input type="text" name="address" id="address" class="form-control" placeholder="Address" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="registration_date">Registration Date</label>
-                                <input type="date" name="registration_date" id="registration_date" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">Save</button>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 @stop
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 @stop
