f83c19d2472827c6145488fc8e83e345e170b71d
Ian Labicani <labicaniian@gmail.com>
2024-06-09 08:25:35 +0800

    feat: : add Patient  model

---
+++ app/Models/Patient.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'gender',
+        'contact_number',
+        'email',
+        'address',
+        'registration_date'
+    ];
+}
