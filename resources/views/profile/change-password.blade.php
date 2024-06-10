ca6c0407fe14245f6cde97c201f2869473a91d99
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-06-11 06:20:47 +0800

    Modify UI

--- resources/views/profile/change-password.blade.php
+++ resources/views/profile/change-password.blade.php
@@ -2,59 +2,146 @@
 
 @section('title', 'Change Password')
 
+@section('css')
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="{{ url('Css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ url('Css/fontawesome.min.css') }}">
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ url('Image/logo.png') }}" type="image/x-icon">
+    {{-- Boostrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+    {{-- Alert  --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <style>
+        body {
+            font-family: 'Nunito'
+        }
+
+        .color {
+            color: #039e43;
+        }
+
+        .colored-toast.swal2-icon-success {
+            background-color: #039e43 !important;
+        }
+
+        .colored-toast.swal2-icon-error {
+            background-color: #f27474 !important;
+        }
+
+        .colored-toast.swal2-icon-warning {
+            background-color: #DC3545 !important;
+        }
+
+        .colored-toast.swal2-icon-info {
+            background-color: #3fc3ee !important;
+        }
+
+        .colored-toast.swal2-icon-question {
+            background-color: #87adbd !important;
+        }
+
+        .colored-toast .swal2-title {
+            color: white;
+        }
+
+        .colored-toast .swal2-close {
+            color: white;
+        }
+
+        .colored-toast .swal2-html-container {
+            color: white;
+        }
+    </style>
+@stop
+
 @section('content_header')
-    <h1>Change Password</h1>
+    <h4 class="fw-bolder">Change Password</h4>
 @stop
 
 @section('content')
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+    @if (session('success'))
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                iconColor: 'white',
+                customClass: {
+                    popup: 'colored-toast',
+                },
+                showConfirmButton: false,
+                timer: 3000,
+                timerPr0ogressBar: true,
+            });
+            (async () => {
+                await Toast.fire({
+                    icon: 'success',
+                    title: 'Password changed successfully'
+                })
+            })()
+        </script>
+    @endif
 
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form action="{{ route('profile.changePassword') }}" method="POST">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('profile.changePassword') }}" method="POST">
         @csrf
         @method('POST')
-        <div class="form-group">
-            <label for="current_password">Current Password</label>
-            <input type="password" name="current_password" id="current_password" class="form-control">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="current_password">Current Password</label>
+                    <input type="password" name="current_password" id="current_password" class="form-control"
+                        placeholder="Enter your current password">
+                </div>
+            </div>
         </div>
+
         @if ($errors->has('current_password'))
             <span class="text-danger">{{ $errors->first('current_password') }}</span>
         @endif
-        <div class="form-group">
-            <label for="new_password">New Password</label>
-            <input type="password" name="new_password" id="new_password" class="form-control">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="new_password">New Password</label>
+                    <input type="password" name="new_password" id="new_password" class="form-control"
+                        placeholder="Enter your new password">
+                </div>
+            </div>
         </div>
         @if ($errors->has('new_password'))
             <span class="text-danger">{{ $errors->first('new_password') }}</span>
         @endif
-        <div class="form-group">
-            <label for="new_password_confirmation">Confirm Password</label>
-            <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="new_password_confirmation">Confirm Password</label>
+                    <input type="password" name="new_password_confirmation" id="new_password_confirmation"
+                        class="form-control" placeholder="Confirm your password">
+                </div>
+            </div>
         </div>
-
         <button type="submit" class="btn btn-primary">Change Password</button>
     </form>
 
 @stop
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
+
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
 @stop
