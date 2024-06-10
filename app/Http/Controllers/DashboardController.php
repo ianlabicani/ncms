392b1c36ea4273e09da81cf878103d3710e87042
Arnold Fernando <junfernando727@gmail.com>
2024-06-10 09:53:10 +0800

    add dashboard controller with logic

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {   $today = Carbon::today();
+        $startOfWeek = Carbon::now()->startOfWeek();
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $startOfYear = Carbon::now()->startOfYear();
+
+        $patientsToday = Patient::whereDate('created_at', $today)->count();
+        $patientsThisWeek = Patient::whereDate('created_at', '>=', $startOfWeek)->count();
+        $patientsThisMonth = Patient::whereDate('created_at', '>=', $startOfMonth)->count();
+        $patientsThisYear = Patient::whereDate('created_at', '>=', $startOfYear)->count();
+
+        return view('home', compact('patientsToday', 'patientsThisWeek', 'patientsThisMonth', 'patientsThisYear'));
+    }
+}
