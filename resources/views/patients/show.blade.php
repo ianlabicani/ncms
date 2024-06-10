16661177fb6597366205cde67f5efb62e63a9b65
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-06-10 23:02:23 +0800

    Modify the Ui and add some scrollable table

--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -28,13 +28,47 @@
 @stop
 
 @section('content_header')
-    <h4 class="fw-bolder">Dashboard</h4>
+    <h4 class="fw-bolder">Show Patient Data</h4>
 @stop
 
 @section('content')
-        <!-- show patient info -->
-        <h1>show patient data here</h1>
-        <h2>patient name: {{$patient->first_name}}</h2>
+    @php
+        use Carbon\Carbon;
+    @endphp
+
+    <body>
+        <div class="row">
+            <div class="col-3">
+                <h6 class="d-inline "><strong>Name:</strong> {{ $patient->first_name }}
+                    {{ $patient->last_name }} </h6>
+            </div>
+            <div class="col-3">
+                <h6 class="d-inline"><strong>Sex:</strong> {{ $patient->gender }} </h6>
+            </div>
+            <div class="col-3">
+                <h6 class="d-inline"><strong>Address:</strong> {{ $patient->address }} </h6>
+            </div>
+        </div>
+        <hr class="mt-0">
+
+        <div class="row pt-2">
+            <div class="col-3">
+                <h6 class="d-inline "><strong>Contact Number:</strong> {{ $patient->contact_number }}</h6>
+            </div>
+            <div class="col-3">
+                <h6 class="d-inline"><strong>Email:</strong> {{ $patient->email }} </h6>
+            </div>
+            <div class="col-3">
+                <h6 class="d-inline"><strong>Date of Birth:</strong>
+                    {{ Carbon::parse($patient->date_of_birth)->format('F j, Y') }} </h6>
+            </div>
+            <div class="col-3">
+                <h6 class="d-inline"><strong>Registration Date:</strong>
+                    {{ Carbon::parse($patient->registration_date)->format('F j, Y') }} </h6>
+            </div>
+        </div>
+        <hr class="mt-0">
+
         <!-- add styling and display patient information -->
 
 
@@ -42,39 +76,57 @@
 
         <form action="{{ route('patient-visit.store') }}" method="POST">
             @csrf
-            <input type="hidden" class="form-control" id="id" name="id" value="{{$patient->id}}">
-            <div class="mb-3">
-                <label for="visit_date" class="form-label">Visit Date</label>
-                <input type="date" class="form-control" id="visit_date" name="visit_date" required>
-            </div>
-            <div class="mb-3">
-                <label for="purpose" class="form-label">Purpose</label>
-                <input type="text" class="form-control" id="purpose" name="purpose" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Add Record</button>
-        </form>
+            <input type="hidden" class="form-control" id="id" name="id" value="{{ $patient->id }}">
+            <div class="row pt-2">
+                <div class="col-4">
 
-        <!-- patient's records table -->
-        <table class="table" border="1px">
-            <thead>
-                <tr>
-                    <th>Visit Date</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($patient->patientVisits as $record)
-                <tr>
-                    <td>{{ $record->visit_date }}</td>
-                    <td>{{ $record->purpose }}</td>
-                </tr>
-                @empty
-                <tr>
-                    <td colspan="2" class="text-center">No records found</td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
+
+                    <div class="mb-3">
+                        <label for="visit_date" class="form-label">Visit Date <small class="text-muted">(Visiting today?
+                                Leave blank)</small></label>
+                        <input type="date" class="form-control" id="visit_date" name="visit_date">
+                    </div>
+                </div>
+                <div class="col-6">
+
+                    <div class="mb-3">
+                        <label for="purpose" class="form-label">Purpose <small class="text-muted">(Purpose of going
+                                clinic)</small></label>
+                        <input type="text" class="form-control" id="purpose" name="purpose" required>
+                    </div>
+                </div>
+                <div class="col-2 pt-2">
+                    <button type="submit" class="btn btn-primary mb-2 mt-4">Add Record</button>
+                </div>
+            </div>
+        </form>
+        {{-- <hr class="mt-0"> --}}
+        <div class="pt-2">
+            <div style="height: 300px; overflow-y: auto;">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Visit Date</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($patient->patientVisits as $record)
+                            <tr>
+                                <td> {{ Carbon::parse($record->visit_date)->format('F j, Y') }}</td>
+                                <td>{{ $record->purpose }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="2" class="text-center">No records found</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </body>
 @stop
 
 @section('js')
