627e28d8248e946e3fec4ce1ca57aa9c0fe957b2
Arnold Fernando <junfernando727@gmail.com>
2024-06-17 10:08:19 +0800

    add activity log model and logic

---
+++ app/Models/ActivityLog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityLog extends Model
+{
+    protected $table = 'activity_log';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'causer_id');
+    }
+
+    protected $casts = [
+        'properties' => 'array',
+    ];
+}
